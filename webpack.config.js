1565194a68fe9c28cc00c53fd9488e96d1ccd63b
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2021-04-04 23:46:36 +0200

    added material

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+function tryResolve_(url, sourceFilename) {
+    // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
+    // when the importer throws
+    try {
+        return require.resolve(url, { paths: [path.dirname(sourceFilename)] });
+    } catch (e) {
+        return '';
+    }
+}
+
+function tryResolveScss(url, sourceFilename) {
+    // Support omission of .scss and leading _
+    const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+    return tryResolve_(normalizedUrl, sourceFilename) ||
+        tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+            sourceFilename);
+}
+
+function materialImporter(url, prev) {
+    if (url.startsWith('@material')) {
+        const resolved = tryResolveScss(url, prev);
+        return { file: resolved || url };
+    }
+    return { file: url };
+}
+
+
+module.exports = [{
+    entry: './sass/main.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'style-bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'theme.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer()
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            },
+                            // Prefer Dart Sass
+                            implementation: require('sass'),
+
+                            // See https://github.com/webpack-contrib/sass-loader/issues/804
+                            webpackImporter: false,
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+}];
