940453159868a47c622f96b8595942457f4382ab
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2021-04-05 16:45:50 +0200

    build script fix

--- build.ps1
+++ build.ps1
@@ -5,13 +5,14 @@ Remove-Item "tmp" -Recurse -ErrorAction Ignore
 
 npm run build-material
 
-mkdir $folder
-Copy-Item -Path ".\keycloak\login" -Destination "tmp\$($folder)\login" -Recurse
+mkdir "tmp\$($folder)"
+Copy-Item -Path ".\src\keycloak-template\login" -Destination "tmp\$($folder)\login" -Recurse
 Copy-Item -Path ".\dist\favicon\" -Destination "tmp\$($folder)\login\resources\img" -Recurse
 Copy-Item -Path ".\src\icons\logo.svg" -Destination "tmp\$($folder)\login\resources\img\logo.svg"
 Copy-Item -Path ".\dist\material-components-web.min.css" -Destination "tmp\$($folder)\login\resources\css"
 
+mkdir "build"
 Compress-Archive -Path "tmp\$($folder)" -DestinationPath "build\$($folder).zip"
 
-Remove-Item $tmp -Recurse -ErrorAction Ignore
+Remove-Item "tmp" -Recurse -ErrorAction Ignore
 Remove-Item "dist" -Recurse -ErrorAction Ignore
