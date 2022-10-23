2fe773b1cef4b339c30262b163740af6628d025b
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2022-10-23 20:54:54 +0200

    added template bg color in template customization

--- build.ps1
+++ build.ps1
@@ -2,6 +2,8 @@ $folder = 'out'
 
 Remove-Item "dist" -Recurse -ErrorAction Ignore
 Remove-Item "tmp" -Recurse -ErrorAction Ignore
+Remove-Item "build" -Recurse -ErrorAction Ignore
+Remove-Item $folder -Recurse -ErrorAction Ignore
 
 npm run build-material
 
@@ -9,7 +11,7 @@ mkdir "tmp\$($folder)"
 Copy-Item -Path ".\src\keycloak-template\login" -Destination "tmp\$($folder)\login" -Recurse
 Copy-Item -Path ".\dist\favicon\" -Destination "tmp\$($folder)\login\resources\img" -Recurse
 Copy-Item -Path ".\src\icons\logo.svg" -Destination "tmp\$($folder)\login\resources\img\logo.svg"
-mkdir "tmp\$($folder)\login\resources\css"
+mkdir -ErrorAction Ignore "tmp\$($folder)\login\resources\css"
 Copy-Item -Path ".\dist\material-components-web.min.css" -Destination "tmp\$($folder)\login\resources\css"
 
 mkdir "build"
